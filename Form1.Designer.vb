deffa78357e1ac4aa683e5f61869ae3378f304b8
PatoFlamejanteTV <flamejantepato77@gmail.com>
2025-01-01 19:28:36 -0300

    versão early-alpha

---
+++ Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MENU
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LOGOTITLE = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LOGOTITLE
+        '
+        Me.LOGOTITLE.AutoSize = True
+        Me.LOGOTITLE.Font = New System.Drawing.Font("Comic Sans MS", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LOGOTITLE.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LOGOTITLE.Location = New System.Drawing.Point(21, 9)
+        Me.LOGOTITLE.Name = "LOGOTITLE"
+        Me.LOGOTITLE.Size = New System.Drawing.Size(305, 147)
+        Me.LOGOTITLE.TabIndex = 0
+        Me.LOGOTITLE.Text = "Bostil" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  SIMULATOR" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "    2 0 2 5 !11!1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Franklin Gothic Book", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 280)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(323, 49)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "JOGAR (huehuehuehuehue)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MENU
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LOGOTITLE)
+        Me.Name = "MENU"
+        Me.Text = "MENU"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LOGOTITLE As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class MENU
+    Private WithEvents backgroundWorker As New System.ComponentModel.BackgroundWorker
+    Private currentColorIndex As Integer = 0
+
+    Private Sub MENU_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        backgroundWorker.WorkerReportsProgress = False
+        AddHandler backgroundWorker.DoWork, AddressOf BackgroundWorker_DoWork
+        AddHandler backgroundWorker.RunWorkerCompleted, AddressOf BackgroundWorker_RunWorkerCompleted
+        backgroundWorker.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs)
+        Dim rainbowColors As Color() = {
+            Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Violet
+        }
+
+        Do
+            For I As Integer = 200 To 255
+                ' Calculate color components based on the current rainbow color
+                Dim currentColor As Color = rainbowColors(currentColorIndex)
+                Dim red As Integer = currentColor.R * I / 255
+                Dim green As Integer = currentColor.G * I / 255
+                Dim blue As Integer = currentColor.B * I / 255
+
+                ' Simulate work to make the effect smoother
+                System.Threading.Thread.Sleep(1)
+
+                ' Update the UI control on the main thread using Invoke
+                Me.Invoke(Sub()
+                              LOGOTITLE.ForeColor = Color.FromArgb(red, green, blue)
+                          End Sub)
+            Next
+
+            ' Move to the next color in the rainbow
+            currentColorIndex = (currentColorIndex + 1) Mod rainbowColors.Length
+
+        Loop
+    End Sub
+
+    Private Sub BackgroundWorker_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs)
+        ' Optional: Handle completion (e.g., enable controls)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i As Integer = 100 To 0 Step -1 ' Decrement i from 100 to 0
+            System.Threading.Thread.Sleep(10)
+            Me.Invoke(Sub()
+                          Me.Opacity = i / 100.0
+                      End Sub)
+        Next
+    End Sub
+End Class
